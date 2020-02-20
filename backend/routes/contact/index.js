ee061c0739aa42b71d0a7d249d15de1c3779c681
Tommy Shi <tommyshi19991130@gmail.com>
2020-02-20 20:40:45 +1300

    Write api logic for contact email

--- backend/routes/contact/index.js
+++ backend/routes/contact/index.js
@@ -1,8 +1,33 @@
 const router = require('express').Router();
+const nodemailer = require('nodemailer');
+const dotenv = require('dotenv').config();
+
+var smtpTransport = nodemailer.createTransport({
+    service: "gmail",
+    host: "smtp.gmail.com",
+    auth: {
+        user: process.env.GMAIL,
+        pass: process.env.GMAIL_PASSWORD
+    }
+});
 
 module.exports = (function() {
     router.post('/', (req, res) => {
+        const emailText = "name: " + req.body.name + "\nemail: " + req.body.email + "\nphone: " + req.body.phone +
+            "\nmessage: " + req.body.message;
+        var mailOption = {
+            to: process.env.GMAIL,
+            subject: "New Message from Website",
+            text: emailText
+        }
 
+        smtpTransport.sendMail(mailOption, (err, res) => {
+            if (err) {
+                return res.status(500).json({ success: false, error: err });
+            } else {
+                return res.json({ success: true });
+            }
+        });
     });
 
     return router;
