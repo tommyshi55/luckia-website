6c7a8189ed4b9b05c7c865b7f3f9f8da669b1823
Tommy Shi <tommyshi19991130@gmail.com>
2020-02-28 22:18:46 +1300

    Remove backend database connection since not needed

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const mongoose = require('mongoose');
 
 const PORT = 4000;
 const app = express();
@@ -9,11 +8,6 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-mongoose.connect('mongodb://127.0.0.1:27017/luckia-sh-members', { useNewUrlParser: true });
-const db = mongoose.connection;
-db.once('open', () => console.log("Connected to the database"));
-db.on('error', (err) => console.log("Database connection error " + err)); 
 
 app.use('/api/signup', require('./routes/signup'));
 app.use('/api/contact', require('./routes/contact'));
