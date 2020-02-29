d73bf60280c33fcd2f6e6f10cea0644a17b07ddd
Tommy Shi <tommyshi19991130@gmail.com>
2020-03-01 11:55:42 +1300

    build

---
+++ frontend/build/precache-manifest.3fbae89accba70b7f63c5605467606a3.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "349ec6c159aa2e7835677a80d28dd946",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a57756114a8434adad7",
+    "url": "/static/css/main.08666bbe.chunk.css"
+  },
+  {
+    "revision": "9d4aaadd92bd83233d45",
+    "url": "/static/js/2.77ddeb15.chunk.js"
+  },
+  {
+    "revision": "9a57756114a8434adad7",
+    "url": "/static/js/main.e4b74591.chunk.js"
+  },
+  {
+    "revision": "65661ab860b4ce0cd825",
+    "url": "/static/js/runtime~main.d9c0d5ac.js"
+  },
+  {
+    "revision": "0b37f7db5bf7d7c1b023360ed53db140",
+    "url": "/static/media/bg5.0b37f7db.jpg"
+  },
+  {
+    "revision": "013b04881a6177ff0648349533ef0447",
+    "url": "/static/media/bg7.013b0488.jpg"
+  },
+  {
+    "revision": "8cfdd67a8ad34bc88343e1889d7b7255",
+    "url": "/static/media/bg8.8cfdd67a.jpg"
+  },
+  {
+    "revision": "7d2facc52e51e6dd0969bd739e767ccb",
+    "url": "/static/media/bg9.7d2facc5.jpg"
+  },
+  {
+    "revision": "565b632919d2b721800f8be36eac020f",
+    "url": "/static/media/byron.565b6329.png"
+  },
+  {
+    "revision": "edc8bf3ab87b411f114c414a37ea3929",
+    "url": "/static/media/church.edc8bf3a.png"
+  },
+  {
+    "revision": "5b7c1cad66a2efdbf3604615cc9d7d1c",
+    "url": "/static/media/clint-mckoy.5b7c1cad.jpg"
+  },
+  {
+    "revision": "b8f8e85e2180945c9cf388d027b3371d",
+    "url": "/static/media/cynthia.b8f8e85e.jpg"
+  },
+  {
+    "revision": "6004580b26fa04e3614ec0dc4bef555f",
+    "url": "/static/media/event1.6004580b.jpg"
+  },
+  {
+    "revision": "846f2674c9394484f9d0d43dcdef9cfd",
+    "url": "/static/media/event2.846f2674.JPG"
+  },
+  {
+    "revision": "5e6204922142b01c30c39b110d674843",
+    "url": "/static/media/event3.5e620492.JPG"
+  },
+  {
+    "revision": "2b83560917bbb144edb2c52961d76271",
+    "url": "/static/media/event4.2b835609.JPG"
+  },
+  {
+    "revision": "a217bb2a89c97dee32a061b7ecd2ee78",
+    "url": "/static/media/event5.a217bb2a.jpg"
+  },
+  {
+    "revision": "ce50f837770372d4d1de80e80f330b7e",
+    "url": "/static/media/hongchang.ce50f837.JPG"
+  },
+  {
+    "revision": "dc3d3f789f5507fa46c37bfeb0d568ff",
+    "url": "/static/media/lexie.dc3d3f78.jpg"
+  },
+  {
+    "revision": "9bc112eb44bb49802d1470d6d565dd30",
+    "url": "/static/media/lucy.9bc112eb.jpg"
+  },
+  {
+    "revision": "75220977d4c0956ebd446d23c0945d26",
+    "url": "/static/media/raymond.75220977.JPG"
+  },
+  {
+    "revision": "6a8d78a2466ea8d0a3706bfb492b2e5d",
+    "url": "/static/media/service1.6a8d78a2.JPG"
+  },
+  {
+    "revision": "3d243d752973aa86b58d6a751fab052c",
+    "url": "/static/media/service2.3d243d75.JPG"
+  },
+  {
+    "revision": "3a66c88ff86eb5b1e397f2b65f72f5f6",
+    "url": "/static/media/service3.3a66c88f.JPG"
+  },
+  {
+    "revision": "554baaa12304a3972626b7021669cc8e",
+    "url": "/static/media/tommy.554baaa1.jpg"
+  }
+]);
