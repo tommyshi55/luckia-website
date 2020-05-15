3daa7dd861d649b165e6073065f9cbe6e3338689
Tommy Shi <tommyshi19991130@gmail.com>
2020-05-15 22:51:32 +1200

    process.env.port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-const PORT = process.env || 4000;
+const PORT = process.env.PORT || 4000;
 const app = express();
 
 app.use(cors());
